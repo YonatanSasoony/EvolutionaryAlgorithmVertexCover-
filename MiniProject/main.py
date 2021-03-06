02f2fea4bf7ab907dafe8d27c7e28618b37c954d
YonatanSasoony <ysasony@gmail.com>
2021-03-06 17:28:04 +0200

    version1

---
+++ MiniProject/main.py
@@ -0,0 +1,244 @@
+import pandas as pd
+import numpy as np
+import random
+import math
+import gc
+import matplotlib.pylab as plt
+import networkx as nx
+
+import time
+start_time = time.time()
+
+nodes = 250
+adjacency_matrix = np.zeros((nodes, nodes), dtype=np.int)
+print(adjacency_matrix.shape)
+
+edge_probability = .0085
+
+edges = []
+edges_cnt = 0
+for i in range(nodes):
+    for j in range(i):
+        prob = random.random()
+        if prob < edge_probability:
+            adjacency_matrix[i, j] = 1
+            edges.append((i, j))
+            edges_cnt += 1
+
+print(adjacency_matrix)
+
+print(edges[:10])
+print("Number of edges {}".format(edges_cnt))
+
+G = nx.Graph()
+G.add_nodes_from(list(range(0, nodes)))
+G.add_edges_from(edges)
+
+plt.figure(figsize=(12, 6))
+nx.draw(G, node_color='r', node_size=18, alpha=0.8)
+plt.show()
+
+# Vertex Cover Greedy Algorithm
+visited = np.zeros(nodes)
+cnt = 0
+for e in edges:
+    (u, v) = e
+    if (visited[u] == 0) & (visited[v] == 0):
+        visited[u] = 1
+        visited[v] = 1
+        cnt += 2
+
+print("Vertex cover consists of {} nodes".format(cnt))
+
+approximation_algo_result = cnt
+print(approximation_algo_result)
+
+n = nodes
+pop_total = int(50 * max(1, round(n / 5.0)))  # max population allowed in the environment
+pop_init = int(20 * max(1, round(n / 5.0)))
+max_iterate = int(7 * max(1, round(n / 5.0)))
+
+print("N = {}\nPopulation Total = {}\nPopulation Initial = {}\nMax Iteration = {}\n".format(n, pop_total, pop_init,
+                                                                                            max_iterate))
+
+def chromosomes_gen(n, k, pop_init):
+    lst = []
+    for i in range(pop_init):
+        chromosome = np.zeros(n, dtype=int)
+        samples = random.sample(range(0, n), k=k)
+        for j in range(k):
+            chromosome[samples[j]] = 1
+        #         print(chromosome)
+        lst.append(chromosome)
+    return lst
+
+
+def cost(cmbn, n, edges):
+    obstacles = 0
+    for e in edges:
+        (u, v) = e
+        if ((cmbn[u] == 0) & (cmbn[v] == 0)):
+            obstacles += 1
+    return obstacles
+
+
+def selection(lst, pop_total, n, edges):
+    score = []
+    output_lst = []
+    len_lst = len(lst)
+    for i in range(len_lst):
+        score.append(cost(lst[i], n, edges))
+    sorted_index = np.argsort(score)
+    cnt = 0
+    for i in range(len_lst):
+        output_lst.append(lst[sorted_index[i]])
+        if ((i + 1) == pop_total):
+            break
+    lst = output_lst
+    return lst, score[sorted_index[0]]
+
+
+def helper_print(lst, n):
+    res = []
+    for i in range(n):
+        if lst[i] == 1:
+            res.append(i)
+    print(res)
+
+
+def cross_over_mutate_extended(lst, n, k, mutat_prob, pop_total, edges):
+    new_lst = lst.copy()
+    len_lst = len(lst)
+    cross_over_prob = 0.50
+    mutat_prob = 0.05
+    variations = 1
+
+    # Crossover
+    for i in range(len_lst):
+        for v in range(variations):
+            tmp = lst[i].copy()
+
+            mate_with = lst[int(random.uniform(0, len_lst))]
+
+            tmp_unique = []
+            mate_with_unique = []
+
+            for j in range(n):
+                if (tmp[j] == 1):
+                    tmp_unique.append(j)
+                if (mate_with[j] == 1):
+                    mate_with_unique.append(j)
+
+            tmp_unique = np.setdiff1d(tmp, mate_with)
+            random.shuffle(tmp_unique)
+            mate_with_unique = np.setdiff1d(mate_with, tmp)
+            random.shuffle(mate_with_unique)
+
+            swap = math.ceil(cross_over_prob * min(len(tmp_unique), len(mate_with_unique)))
+
+            for j in range(swap):
+                tmp[mate_with_unique[j]] = 1
+                tmp[tmp_unique[j]] = 0
+
+            # Mutation
+            zeroes = []
+            ones = []
+            for j in range(n):
+                if tmp[j] == 1:
+                    ones.append(j)
+                else:
+                    zeroes.append(j)
+
+            random.shuffle(ones)
+            random.shuffle(zeroes)
+
+            coin_toss = random.random()
+            if (coin_toss <= 0.5):
+                swaps = min(len(ones), len(zeroes))
+
+                for j in range(swaps):
+                    coin_toss2 = random.random()
+                    if (coin_toss2 < mutat_prob):
+                        tmp[ones[j]] = 0
+                        tmp[zeroes[j]] = 1
+                        # Swapping logic
+                        dummy = ones[j]
+                        ones[j] = zeroes[j]
+                        zeroes[j] = dummy
+            else:
+
+                mutate_lst = []
+                for e in edges:
+                    (u, v) = e
+                    if ((tmp[u] == 0) & (tmp[v] == 0)):
+                        coin_toss2 = random.random()
+                        if (coin_toss2 < mutat_prob):
+                            coin_toss3 = random.random()
+                            if (coin_toss3 <= 0.5):
+                                if (u not in mutate_lst):
+                                    mutate_lst.append(u)
+                            else:
+                                if (v not in mutate_lst):
+                                    mutate_lst.append(v)
+
+                random.shuffle(mutate_lst)
+                sz = min(len(ones), len(mutate_lst))
+
+                for j in range(sz):
+                    tmp[ones[j]] = 0
+                    tmp[mutate_lst[j]] = 1
+                    # Swapping logic
+                    dummy = ones[j]
+                    ones[j] = mutate_lst[j]
+                    mutate_lst[j] = dummy
+
+            new_lst.append(tmp)
+
+    return new_lst
+
+
+def environment(n, k, mutat_prob, pop_init, pop_total, max_iterate, edges):
+    lst = chromosomes_gen(n, k, pop_init)
+    for it in range(max_iterate):
+        lst = cross_over_mutate_extended(lst, n, k, mutat_prob, pop_total, edges)
+        #         return
+        lst, cost_value = selection(lst, pop_total, n, edges)
+        if (it % 10) == 9:
+            print("k = {}, Iteration = {}, Cost = {}".format(k, it + 1, cost_value))
+        if cost_value == 0:
+            break
+    result = []
+    soln = lst[0]
+    for j in range(len(soln)):
+        if (soln[j] == 1):
+            result.append(j)
+    print("k = {}, Iteration = {}, Cost = {}\nSoln = {}".format(k, it, cost_value, result))
+    return cost_value, result
+
+
+def free_memory():
+    gc.collect()
+
+
+def mfind(n, mutat_prob, pop_init, pop_total, max_iterate, edges, start, end):
+    result_dict = {}
+    l = start
+    h = end
+    ans = 0
+    while (l <= h):
+        m = int((l + h) / 2.0)
+        cost_value, result = environment(n, m, mutat_prob, pop_init, pop_total, max_iterate, edges)
+        #         print("Cost is {} result is {}".format(cost_value,result))
+        if (cost_value == 0):
+            result_dict[m] = result
+            h = m - 1
+        else:
+            l = m + 1
+    return result_dict
+
+
+free_memory()
+result = mfind(n, 0.05, pop_init, pop_total, max_iterate, edges, int(approximation_algo_result / 2), n)
+
+print(result.keys())
+print("My program took" + str(time.time() - start_time) + "to run")
---
+++ MiniProject/version1.py
@@ -0,0 +1,169 @@
+import matplotlib.pyplot as plt
+import random
+import copy
+import networkx as nx
+import numpy as np
+
+def generate_string(length):
+    binary_str = ""
+    for i in range(0, length):
+        next_bit = str(random.randint(0, 1))
+        binary_str += next_bit
+    return binary_str
+
+
+# def fitness(s, graph):
+#     n = len(graph)
+#     for i in range(0, n):
+#         for j in range(0, n):
+#             if graph[i][j] == 1 and int(s[i]) == 0 and int(s[j]) == 0:
+#                 return 1
+#     k = 0
+#     for i in range(0, n):
+#         k += int(s[i])
+#     return n - k + 10
+
+
+def fitness(s, graph):
+    n = len(graph)
+    sum = 1
+    for i in range(0, n):
+        for j in range(i, n):
+            if graph[i][j] == 1:
+                if int(s[i]) == 0 and int(s[j]) == 0:
+                    sum += 0
+                elif int(s[i]) == 1 and int(s[j]) == 1:
+                    sum += 1
+                else:
+                    sum += 2
+
+    return sum
+
+
+def selection(str_fit_list):
+    length = len(str_fit_list)
+    range_list = []
+    sum = 0
+    for c, fit in str_fit_list:
+        range_list += [(c, sum, sum + fit)]
+        sum += fit
+
+    str_fit_list.clear()
+    for i in range(0, length):
+        rand = random.uniform(0, sum)
+        for s, start, end in range_list:
+            if start <= rand <= end:
+                str_fit_list += [(s, 0)]
+    return str_fit_list
+
+
+def crossover(str1, str2):
+    length = len(str1)
+    mid = length / 2
+    child1 = str1[0: int(mid)] + str2[int(mid): int(length)]
+    child2 = str2[0: int(mid)] + str1[int(mid): int(length)]
+    return child1, child2
+
+
+def mutation(s):
+    length = len(s)
+    bit = random.randint(0, length - 1)
+    if s[bit] == "0":
+        return s[0: bit] + "1" + s[bit + 1: length]
+    else:
+        return s[0: bit] + "0" + s[bit + 1: length]
+
+
+def generate_graph(nodes=100):
+    edge_probability = 0.0585
+    adjacency_matrix = np.zeros((nodes, nodes))
+    edges = []
+    edges_cnt = 0
+    for i in range(nodes):
+        for j in range(i):
+            prob = random.random()
+            if prob < edge_probability:
+                adjacency_matrix[i, j] = 1
+                adjacency_matrix[j, i] = 1
+                edges.append((i, j))
+                edges_cnt += 1
+    G = nx.Graph()
+    G.add_nodes_from(list(range(0,nodes)))
+    G.add_edges_from(edges)
+    nx.draw(G,node_color='r', node_size=18, alpha=0.8)
+    plt.show()
+    return adjacency_matrix
+
+
+# def main():
+graph = [[0, 1, 1, 0, 0, 0],
+         [1, 0, 1, 1, 1, 1],
+         [1, 1, 0, 0, 0, 0],
+         [0, 1, 0, 0, 0, 0],
+         [0, 1, 0, 0, 0, 0],
+         [0, 1, 0, 0, 0, 0]]
+
+graph = generate_graph(nodes=50)
+
+# graph = np.zeros((20, 20))
+# for i in range(20):
+#     graph[i, 0] = 1
+#     graph[0, i] = 1
+graph_length = len(graph)
+num_of_covers = 1000  # even number
+num_generations = 1000
+
+max_fitness = 0
+max_cover = ""
+covers = []
+temp = []
+x_gen = []
+y_fit = []
+for i in range(0, num_of_covers):  # covers
+    c = generate_string(graph_length)
+    fit = fitness(c, graph)
+    covers += [(c, fit)]
+    if fit > max_fitness:
+        max_fitness = fit
+        max_cover = c
+for i in range(0, num_generations):  # generations
+    x_gen += [i]
+    y_fit += [max_fitness]
+    covers = selection(covers)  # select the good covers
+    for j in range(0, int(num_of_covers/2)):  # for each pair of covers
+        t1 = covers.pop()[0]
+        t2 = covers.pop()[0]
+        rand1 = random.randint(1, 100)
+        if rand1 <= 70:  # 70% do crossover
+            childrens = crossover(t1, t2)
+            t1 = childrens[0]
+            t2 = childrens[1]
+        rand2 = random.randint(1, 1000)
+        if rand2 == 500:  # 1/1000% do mutation
+            t1 = mutation(t1)
+            t2 = mutation(t2)
+        fit = fitness(t1, graph)
+        if fit > max_fitness:
+            max_fitness = fit
+            max_cover = t1
+        temp += [(t1, fit)]
+        fit = fitness(t2, graph)
+        if fit > max_fitness:
+            max_fitness = fit
+            max_cover = t2
+        temp += [(t2, fit)]
+    covers = copy.deepcopy(temp)
+    temp.clear()
+print("the best cover is: " + str(max_cover))
+plt.plot(x_gen, y_fit)
+plt.xlabel("Generation")
+plt.ylabel("Fitness")
+plt.grid()
+plt.show()
+print(max_cover)
+#return max_cover
+
+
+# if __name__ == "__main__":
+#     main()
+
